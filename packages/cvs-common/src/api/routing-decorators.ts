ccf5fc73bcf1bd017706ee7111cc610939254cbe
Matthew Bell <33056264+matthew2564@users.noreply.github.com>
2025-02-24 08:01:53 +0000

    feat: routing decorators to account for extra optional branch in path (#265)

---
+++ packages/cvs-common/src/api/routing-decorators.ts
@@ -0,0 +1,43 @@
+import { Get, Post, Put } from 'routing-controllers';
+
+export function GET(route: string) {
+	return (target: unknown, propertyKey: string, descriptor: PropertyDescriptor) => {
+		// Apply the route
+		Get(route)(target, propertyKey, descriptor);
+
+		// Additionally, apply the route with the branch name
+		if (process.env.BRANCH_NAME) {
+			Get(`/${process.env.BRANCH_NAME}${route}`)(target, propertyKey, descriptor);
+		}
+
+		return descriptor;
+	};
+}
+
+export function POST(route: RegExp) {
+	return (target: unknown, propertyKey: string, descriptor: PropertyDescriptor) => {
+		// Apply the route
+		Post(route)(target, propertyKey, descriptor);
+
+		// Additionally, apply the route with the branch name
+		if (process.env.BRANCH_NAME) {
+			Post(`/${process.env.BRANCH_NAME}${route}`)(target, propertyKey, descriptor);
+		}
+
+		return descriptor;
+	};
+}
+
+export function PUT(route: string) {
+	return (target: unknown, propertyKey: string, descriptor: PropertyDescriptor) => {
+		// Apply the route
+		Put(route)(target, propertyKey, descriptor);
+
+		// Additionally, apply the route with the branch name
+		if (process.env.BRANCH_NAME) {
+			Put(`/${process.env.BRANCH_NAME}${route}`)(target, propertyKey, descriptor);
+		}
+
+		return descriptor;
+	};
+}
