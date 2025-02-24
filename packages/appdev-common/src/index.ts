29b7a4dac6b49f42402513a8dd201b60b737f8a5
Matthew Bell <33056264+matthew2564@users.noreply.github.com>
2025-02-24 14:08:54 +0000

    feat(cb2-16579): validate express request body decorator method

--- packages/appdev-common/src/index.ts
+++ packages/appdev-common/src/index.ts
@@ -1,4 +1,7 @@
 export * from "./api/http-status-codes";
+
 export * from "./auth/auth-checker";
 export * from "./auth/auth-errors";
 export * from "./auth/verify-jwt";
+
+export * from "./validation/request-body";
