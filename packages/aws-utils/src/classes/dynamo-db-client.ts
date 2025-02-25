593f0c46dc8745e1414bc205643fcab1f4600141
Matthew Bell <33056264+matthew2564@users.noreply.github.com>
2025-02-25 08:29:30 +0000

    feat: helper util for recursive dynamo lookups (#270)

--- packages/aws-utils/src/classes/dynamo-db-client.ts
+++ packages/aws-utils/src/classes/dynamo-db-client.ts
@@ -1,6 +1,12 @@
 import { type AttributeValue, DynamoDBClient, type DynamoDBClientConfig } from '@aws-sdk/client-dynamodb';
 import { fromEnv, fromIni } from '@aws-sdk/credential-providers';
-import { DynamoDBDocumentClient, ScanCommand, type ScanCommandInput } from '@aws-sdk/lib-dynamodb';
+import {
+	DynamoDBDocumentClient,
+	QueryCommand,
+	type QueryCommandInput,
+	ScanCommand,
+	type ScanCommandInput,
+} from '@aws-sdk/lib-dynamodb';
 import { captureAWSv3Client } from 'aws-xray-sdk';
 
 export class DynamoDb {
@@ -45,6 +51,9 @@ export class DynamoDb {
 		scanParams: Partial<ScanCommandInput> = {},
 		clientConfig: Partial<DynamoDBClientConfig> = DynamoDb.defaultConfig
 	): Promise<T[]> {
+		// @TODO: This should be able to be simplified by using recursiveFetch
+		// e.g. return DynamoDb.recursiveFetch(ScanCommand, scanParams, clientConfig);
+
 		const rows: T[] = [];
 
 		let lastEvaluatedKey: Record<string, AttributeValue> | undefined = undefined;
@@ -58,6 +67,42 @@ export class DynamoDb {
 
 		do {
 			const data = await client.send(new ScanCommand(params));
+
+			if (data.Items) rows.push(...(data.Items as T[]));
+
+			lastEvaluatedKey = data.LastEvaluatedKey;
+			params.ExclusiveStartKey = data.LastEvaluatedKey;
+		} while (lastEvaluatedKey);
+
+		return rows;
+	}
+
+	/**
+	 * Perform a recursive fetch of a DynamoDB table using either the Query or Scan command
+	 * @template T
+	 * @param {typeof QueryCommand | typeof ScanCommand} command - The command to use for fetching data
+	 * @param {Partial<QueryCommandInput | ScanCommandInput>} inputParams - The parameters to send to the operation
+	 * @param {Partial<DynamoDBClientConfig>} clientConfig - Defaults to the `eu-west-1` region
+	 * @return {Promise<T[]>}
+	 */
+	static async recursiveFetch<T>(
+		command: typeof QueryCommand | typeof ScanCommand,
+		inputParams: Partial<QueryCommandInput | ScanCommandInput>,
+		clientConfig: Partial<DynamoDBClientConfig> = DynamoDb.defaultConfig
+	): Promise<T[]> {
+		const rows: T[] = [];
+
+		let lastEvaluatedKey: Record<string, AttributeValue> | undefined = undefined;
+
+		const client = DynamoDb.getClient(clientConfig);
+
+		const params = {
+			ExclusiveStartKey: lastEvaluatedKey,
+			...inputParams,
+		} as QueryCommandInput | ScanCommandInput;
+
+		do {
+			const data = await client.send(new command(params));
 
 			if (data.Items) rows.push(...(data.Items as T[]));
 
