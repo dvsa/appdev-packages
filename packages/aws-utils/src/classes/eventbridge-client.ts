e89dce5c9ac38a68217d716b29beee476420e6e2
Matthew Bell <33056264+matthew2564@users.noreply.github.com>
2025-05-02 15:34:44 +0100

    feat: upgrade all packages to node 22

---
+++ packages/aws-utils/src/classes/eventbridge-client.ts
@@ -0,0 +1,21 @@
+import { EventBridgeClient, type EventBridgeClientConfig } from '@aws-sdk/client-eventbridge';
+
+import { fromIni } from '@aws-sdk/credential-providers';
+import { captureAWSv3Client } from 'aws-xray-sdk';
+
+export class Eventbridge {
+	private static readonly defaultConfig: Partial<EventBridgeClientConfig> = {
+		region: 'eu-west-1',
+	};
+
+	static getClient(config: Partial<EventBridgeClientConfig> = Eventbridge.defaultConfig) {
+		if (process.env.USE_CREDENTIALS === 'true') {
+			config.credentials = fromIni();
+		}
+
+		const client = new EventBridgeClient(config);
+
+		// If tracing is enabled, then capture the client with AWS X-Ray
+		return process.env._X_AMZN_TRACE_ID ? captureAWSv3Client(client) : client;
+	}
+}
