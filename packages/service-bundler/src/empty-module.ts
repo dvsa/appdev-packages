4b843cd41da412d825d2284b9ca8039deb5869be
Matthew Bell <33056264+matthew2564@users.noreply.github.com>
2025-05-24 11:59:46 +0100

    chore: remove bundler logs (#333)

--- packages/service-bundler/src/empty-module.ts
+++ packages/service-bundler/src/empty-module.ts
@@ -2,14 +2,12 @@
 const noop =
 	(name) =>
 	(...args) => {
-		console.warn(`[noop] ${name}() called from dummy module`, args);
 		return (..._) => {}; // Safe decorator shape: () => () => {}
 	};
 
 export const OpenAPI = noop('OpenAPI');
 export const LambdaAPI = noop('LambdaAPI');
 export const registerLambdaHandler = (target) => {
-	console.warn('[noop] registerLambdaHandler() called from dummy module');
 	return target;
 };
 export default {};
