77868856067a2ee2f8d4ecb3070b182b89dbee5d
Georgia Margetts <georgia.margetts@dvsa.gov.uk>
2026-06-23 15:18:47 +0100

    feat: adding check for tenant id, multiple client ids etc

--- packages/appdev-common/src/auth/auth-checker.ts
+++ packages/appdev-common/src/auth/auth-checker.ts
@@ -1,84 +1,95 @@
-import type { Request } from "express";
-import type { Action } from "routing-controllers";
-import type { APIGatewayModel } from "../api/api-gateway-model";
-import { HttpStatus } from "../api/http-status-codes";
-import { AuthError } from "./auth-errors";
-import { JwtAuthoriser } from "./verify-jwt";
+import type {Request} from "express";
+import type {Action} from "routing-controllers";
+import type {APIGatewayModel} from "../api/api-gateway-model";
+import {HttpStatus} from "../api/http-status-codes";
+import {AuthError} from "./auth-errors";
+import {JwtAuthoriser} from "./verify-jwt";
 
 export type RoutingControllersRequest = Request & {
-	apiGateway: APIGatewayModel;
+    apiGateway: APIGatewayModel;
 };
 
 // biome-ignore lint/complexity/noStaticOnlyClass: this class will be extended in the future
 export class JWTAuthChecker {
-	/**
-	 * Perform a JWT token verification and role check
-	 * @param {RoutingControllersRequest} request
-	 * @param {string | string[]} roles
-	 * @returns {Promise<boolean>}
-	 */
-	static async execute(
-		{ request }: Action,
-		roles: string | string[] = [],
-	): Promise<boolean> {
-		// if running locally, skip the token auth and role check
-		if (
-			process.env.IS_OFFLINE === "true" &&
-			process.env.FORCE_LOCAL_AUTH !== "true"
-		)
-			return true;
 
-		// extract the token from the request headers
-		const headers = (request as RoutingControllersRequest)?.apiGateway.event
-			.headers;
-		const token = headers?.Authorization || headers?.authorization;
+    /**
+     * Create a new instance of the JWTAuthChecker class
+     * @param clientId - the client id to validate the token against
+     * @param tenantId
+     */
+    public constructor(
+        private readonly clientId: string | null = null,
+        private readonly tenantId: string | null = null
+    ) {}
 
-		// if no token is found, then deny access to resource
-		if (!token || token.trim()?.length === 0) {
-			throw new AuthError(
-				HttpStatus.UNAUTHORIZED,
-				"Missing Authorization header",
-			);
-		}
+    /**
+     * Perform a JWT token verification and role check
+     * @param {RoutingControllersRequest} request
+     * @param {string | string[]} roles
+     * @returns {Promise<boolean>}
+     */
+    async execute(
+        {request}: Action,
+        roles: string | string[] = [],
+    ): Promise<boolean> {
+        // if running locally, skip the token auth and role check
+        if (
+            process.env.IS_OFFLINE === "true" &&
+            process.env.FORCE_LOCAL_AUTH !== "true"
+        )
+            return true;
 
-		// create an instance of the JwtAuthoriser class
-		const authoriser = new JwtAuthoriser();
+        // extract the token from the request headers
+        const headers = (request as RoutingControllersRequest)?.apiGateway.event
+            .headers;
+        const token = headers?.Authorization || headers?.authorization;
 
-		// Validate the token and extract the roles from it
-		const { roles: tokenRoles } = await authoriser.verify(token);
+        // if no token is found, then deny access to resource
+        if (!token || token.trim()?.length === 0) {
+            throw new AuthError(
+                HttpStatus.UNAUTHORIZED,
+                "Missing Authorization header",
+            );
+        }
 
-		// check if a singular or list of roles were passed into the @Authorized decorator & remove nullish values
-		const requiredRoles = (Array.isArray(roles) ? roles : [roles]).filter(
-			(role) => !!role,
-		);
+        // create an instance of the JwtAuthoriser class, passing in clientId & tenantId if present
+        const authoriser = new JwtAuthoriser(this.clientId, this.tenantId);
 
-		// if there are no requiredRoles, then any valid token can access the resource, so return true
-		if (requiredRoles.length === 0) return true;
+        // Validate the token and extract the roles from it
+        const {roles: tokenRoles} = await authoriser.verify(token);
 
-		// if there are no roles in JWT token but roles are required, then deny access
-		if (!tokenRoles || !Array.isArray(tokenRoles) || tokenRoles.length === 0) {
-			throw new AuthError(
-				HttpStatus.UNAUTHORIZED,
-				"No roles found in token",
-				"MISSING_ROLES",
-			);
-		}
+        // check if a singular or list of roles were passed into the @Authorized decorator & remove nullish values
+        const requiredRoles = (Array.isArray(roles) ? roles : [roles]).filter(
+            (role) => !!role,
+        );
 
-		// check if one of the required roles is present in JWT token
-		const success = requiredRoles.some((role) => tokenRoles.includes(role));
+        // if there are no requiredRoles, then any valid token can access the resource, so return true
+        if (requiredRoles.length === 0) return true;
 
-		if (!success) {
-			throw new AuthError(
-				HttpStatus.UNAUTHORIZED,
-				"Insufficient permissions",
-				"UNAUTHORIZED",
-				{
-					required: requiredRoles,
-					actual: tokenRoles,
-				},
-			);
-		}
+        // if there are no roles in JWT token but roles are required, then deny access
+        if (!tokenRoles || !Array.isArray(tokenRoles) || tokenRoles.length === 0) {
+            throw new AuthError(
+                HttpStatus.UNAUTHORIZED,
+                "No roles found in token",
+                "MISSING_ROLES",
+            );
+        }
 
-		return true;
-	}
+        // check if one of the required roles is present in JWT token
+        const success = requiredRoles.some((role) => tokenRoles.includes(role));
+
+        if (!success) {
+            throw new AuthError(
+                HttpStatus.UNAUTHORIZED,
+                "Insufficient permissions",
+                "UNAUTHORIZED",
+                {
+                    required: requiredRoles,
+                    actual: tokenRoles,
+                },
+            );
+        }
+
+        return true;
+    }
 }
--- packages/appdev-common/src/auth/verify-jwt.ts
+++ packages/appdev-common/src/auth/verify-jwt.ts
@@ -1,68 +1,77 @@
 import {
-	type JWTPayload,
-	type JWTVerifyOptions,
-	createRemoteJWKSet,
-	jwtVerify,
+    type JWTPayload,
+    type JWTVerifyOptions,
+    createRemoteJWKSet,
+    jwtVerify,
 } from "jose";
-import { HttpStatus } from "../api/http-status-codes";
-import { AuthError } from "./auth-errors";
+import {HttpStatus} from "../api/http-status-codes";
+import {AuthError} from "./auth-errors";
 
 export class JwtAuthoriser {
-	private readonly clientId: string | null;
-	private static readonly ENV = process.env.environment?.toUpperCase() ?? "";
-	private static readonly tokenExpiryEnvExclusionList = [
-		"DEVELOPMENT",
-		"NON-PROD",
-	];
-	private static readonly JWKS_URI = new URL(
-		"https://login.microsoftonline.com/common/discovery/keys",
-	);
-	private static JWKS = createRemoteJWKSet(JwtAuthoriser.JWKS_URI);
+    private readonly clientIds: string | null;
+    private readonly tenantId: string | null;
+    private static readonly ENV = process.env.environment?.toUpperCase() ?? "";
+    private static readonly tokenExpiryEnvExclusionList = [
+        "DEVELOPMENT",
+        "NON-PROD",
+    ];
+    private static readonly JWKS_URI = new URL(
+        "https://login.microsoftonline.com/common/discovery/keys",
+    );
+    private static JWKS = createRemoteJWKSet(JwtAuthoriser.JWKS_URI);
 
-	/**
-	 * Create a new instance of the JwtAuthoriser class
-	 * @param clientId - the client id to validate the token against
-	 */
-	public constructor(clientId: string | null = null) {
-		this.clientId = clientId;
-	}
+    /**
+     * Create a new instance of the JwtAuthoriser class
+     * @param clientIds
+     * @param tenantId
+     */
+    public constructor(clientIds: string | null = null, tenantId: string | null = null) {
+        this.clientIds = clientIds;
+        this.tenantId = tenantId;
+    }
 
-	/**
-	 * Validate a JWT and return the decoded payload
-	 * @param {string} token - the JWT token to validate
-	 * @returns {Promise<JWTPayload>}
-	 */
-	public async verify(token: string): Promise<JWTPayload> {
-		try {
-			const opts: JWTVerifyOptions = {
-				clockTolerance: 10,
-				algorithms: ["RS256"],
-			};
+    /**
+     * Validate a JWT and return the decoded payload
+     * @param {string} token - the JWT token to validate
+     * @returns {Promise<JWTPayload>}
+     */
+    public async verify(token: string): Promise<JWTPayload> {
+        try {
+            const opts: JWTVerifyOptions = {
+                clockTolerance: 10,
+                algorithms: ["RS256"],
+            };
 
-			// issuer validation is handled automatically if present in token
-			if (this.clientId) {
-				opts.audience = this.clientId;
-			}
+            // audience validation is handled automatically if present in token
+            if (this.clientIds?.length) {
+                opts.audience = this.clientIds.split(",").map((id) => id.trim()).filter(Boolean)
+            }
 
-			if (
-				JwtAuthoriser.tokenExpiryEnvExclusionList.includes(JwtAuthoriser.ENV)
-			) {
-				opts.maxTokenAge = Number.POSITIVE_INFINITY;
-			}
+            // issuer validation is handled automatically if present in token
+            if (this.tenantId) {
+				opts.issuer = [
+					`https://sts.windows.net/${this.tenantId}/`,
+					`https://login.microsoftonline.com/${this.tenantId}/v2.0`,
+				];
+            }
 
-			const { payload } = await jwtVerify(token, JwtAuthoriser.JWKS, opts);
+            if (JwtAuthoriser.tokenExpiryEnvExclusionList.includes(JwtAuthoriser.ENV)) {
+                opts.maxTokenAge = Number.POSITIVE_INFINITY;
+            }
 
-			return payload;
-		} catch (err) {
-			const error = err as { code?: string };
+            const {payload} = await jwtVerify(token, JwtAuthoriser.JWKS, opts);
 
-			const code = "code" in error ? error.code : "";
+            return payload;
+        } catch (err) {
+            const error = err as { code?: string };
 
-			throw new AuthError(
-				HttpStatus.UNAUTHORIZED,
-				(err as Error).message,
-				code,
-			);
-		}
-	}
+            const code = "code" in error ? error.code : "";
+
+            throw new AuthError(
+                HttpStatus.UNAUTHORIZED,
+                (err as Error).message,
+                code,
+            );
+        }
+    }
 }
